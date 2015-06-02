69e923fa527894d8ed9b14012d679443cd885f1f
Amie Wilt <amie.wilt@gmail.com>
2015-06-01 21:41:59 -0400

    added validation for start and end years

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,2 +1,9 @@
 class School < ActiveRecord::Base
+  validate :validate_end_year_before_start_year
+
+  def validate_end_year_before_start_year
+    if end_year && start_year
+      errors.add(:end_year, " - Your end date must be prior to your start date.") if end_year < start_year
+    end
+  end
 end
