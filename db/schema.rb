dd3dd65c228f9bb5eff59228cd0cb577f796f51e
Amie Wilt <amie.wilt@gmail.com>
2015-06-01 20:00:32 -0400

    added home controller and view

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150601193439) do
+ActiveRecord::Schema.define(version: 20150601200852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "homes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "life_events", force: :cascade do |t|
     t.string   "title"
