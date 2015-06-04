d7867fa92a0b957e6346f56077076fa2a856ef09
Amie Wilt <amie.wilt@gmail.com>
2015-06-03 22:16:27 -0400

    added new and form views for people controller, added on to people controller, new people aren't showing up in views after being created -- blank space

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,4 +1,5 @@
 class PeopleController < ApplicationController
+  before_action :set_person, only: [:show, :edit, :update, :destroy]
 
   def index
     @people = Person.all
@@ -16,5 +17,49 @@ class PeopleController < ApplicationController
   end
 
   def create
+    @person = Person.new(@person_params)
+
+    respond_to do |format|
+      if @person.save
+        format.html { redirect_to @person, notice: 'Person was successfully created.' }
+        format.json { render :show, status: :created, location: @person }
+      else
+        format.html { render :new }
+        format.json { render json: @person.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @person.update(people_params)
+        format.html { redirect_to @person, notice: 'Person was successfully updated.' }
+        format.json { render :show, status: :ok, location: @person }
+      else
+        format.html { render :edit }
+        format.json { render json: @person.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /life_events/1
+  # DELETE /life_events/1.json
+  def destroy
+    @person.destroy
+    respond_to do |format|
+      format.html { redirect_to people_url, notice: 'Person was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_person
+    @person = Person.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def person_params
+    params.require(:person).permit(:name)
   end
 end
